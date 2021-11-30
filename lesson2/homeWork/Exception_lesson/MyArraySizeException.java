20a2d99bb1ffed47aeb8a08d8cd75748040092cf
Kononovich Denis <den.kononovich@yandex.by>
2021-11-30 09:08:10 +0300

    lesson 2 homework

---
+++ lesson2/homeWork/Exception_lesson/MyArraySizeException.java
@@ -0,0 +1,14 @@
+/**
+ * JavaCore. Homework 2. MyArraySizeException
+ *
+ * @author Denis Kononovich
+ * @version 29.11.21
+ */
+
+package ru.geekbrains.qa.java2.lesson2.homeWork.Exception_lesson;
+
+public class MyArraySizeException extends Exception {
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+}
